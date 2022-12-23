685518c65fb3f3044a4574546f95e293fc56c1f6
Jakub Sajko <jakubsajko.inf@gmail.com>
2022-12-23 12:35:33 +0100

    [BT2010NF-008]: Connect dataCMS to app. Install graphql-hooks package.

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,27 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux/es/exports";
 import { store } from "./app/store";
+import { GraphQLClient, ClientContext } from "graphql-hooks";
+
+const URL = "https://graphql.datocms.com/";
+
+const client = new GraphQLClient({
+  url: URL,
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_DATOCMS_API_TOKEN}`,
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ClientContext.Provider value={client}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ClientContext.Provider>
   </React.StrictMode>
 );
 
