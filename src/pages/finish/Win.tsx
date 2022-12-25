42f5088eda7fa753cb180f7b13e24cf722274121
Jakub Sajko <jakubsajko.inf@gmail.com>
2022-12-25 14:42:57 +0100

    [BT2010NF-011]: Create 2 component - win and failure. Create routes for new components.

---
+++ src/pages/finish/Win.tsx
@@ -0,0 +1,11 @@
+export default function Win() {
+  return (
+    <div className="w-full h-screen bg-win bg-center flex flex-col justify-center items-center">
+      <h1 className="text-lg font-bold italic text-white">Gratulację!</h1>
+      <p className="max-w-lg font-bold text-center italic text-white">
+        Udało ci się odratować historię. Mam nadzieję, że dobrze się bawiłaś/eś
+        rozwiązując quiz.
+      </p>
+    </div>
+  );
+}
